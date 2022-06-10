7e2834bcc0ff45512b2aa74a98c756bfbdd7ca5d
eric-michael <hicks.ericmichael@gmail.com>
2022-06-09 21:45:20 -0400

    cleanup

--- root/client/src/App.js
+++ root/client/src/App.js
@@ -1,20 +1,18 @@
-import './App.css';
-import image from './images/image0.jpg';
+import "./App.css";
+import image from "./images/image0.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="App-header-left">
-          Tacono Gate
-        </div>
+        <div className="App-header-left">Tacono Gate</div>
         <div className="App-header-right">
           <span className="App-header-right-unit">Dates</span>
           <span className="App-header-right-unit">Merch</span>
         </div>
       </header>
       <div className="App-content">
-        <img src={image} alt="Tacono Gate" className="main-image"/>
+        <img src={image} alt="Tacono Gate" className="main-image" />
       </div>
     </div>
   );

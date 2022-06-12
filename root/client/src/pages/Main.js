f47454c790ac25dd993aa6de49c74dc29a54f689
eric-michael <hicks.ericmichael@gmail.com>
2022-06-12 13:56:17 -0400

    Styles for mobile

--- root/client/src/pages/Main.js
+++ root/client/src/pages/Main.js
@@ -4,13 +4,13 @@ import image from "../images/image0.jpg";
 function Main() {
   return (
     <div className="main">
-      <div>
+      <div className="main-image-container">
         <img src={image} alt="Tacono Gate" className="main-image" />
       </div>
 
       <div className="main-playlist">
         <iframe
-          title="test"
+          title="playlist"
           src="https://open.spotify.com/embed/artist/2R72laeotc3lBpfYh4sLAz?utm_source=generator"
           width="100%"
           height="380"

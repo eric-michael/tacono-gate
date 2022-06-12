ed083fc918c7ff9d544a03d9ab1df08c0e842a92
eric-michael <hicks.ericmichael@gmail.com>
2022-06-12 13:17:15 -0400

    Make text links

--- root/client/src/App.js
+++ root/client/src/App.js
@@ -1,22 +1,25 @@
 import { Route, Routes } from "react-router";
+import { Link } from "react-router-dom";
 
 import "./App.css";
 
 import Main from "./pages/Main";
+import Dates from "./pages/Dates";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="App-header-left">Tacono Gate</div>
+        <Link to="/" className="App-header-left">Tacono Gate</Link>
         <div className="App-header-right">
-          <span className="App-header-right-unit">Dates</span>
+          <Link to="/dates" className="App-header-right-unit">Dates</Link>
           <span className="App-header-right-unit">Merch</span>
         </div>
       </header>
       <div className="App-content">
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/dates" element={<Dates />} />
         </Routes>
       </div>
     </div>
